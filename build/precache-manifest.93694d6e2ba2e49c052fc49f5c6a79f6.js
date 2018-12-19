4d3da8bd07aac89b9c750af70e07dc8fd13c3097
smovzhsergey <smovzhsergey@gmail.com>
2018-12-19 11:53:33 +0200

    add build

---
+++ build/precache-manifest.93694d6e2ba2e49c052fc49f5c6a79f6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36edc495b3a842cc97d1",
+    "url": "/static/js/main.36edc495.chunk.js"
+  },
+  {
+    "revision": "0b33b2d539434f02db05",
+    "url": "/static/js/1.0b33b2d5.chunk.js"
+  },
+  {
+    "revision": "5f3a8d44f4e084e68de6a221da9c25e3",
+    "url": "/index.html"
+  }
+];
