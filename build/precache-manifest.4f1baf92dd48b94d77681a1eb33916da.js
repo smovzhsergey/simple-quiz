a5db9a00c9d789de6133dcc3c6f3239afad000f0
smovzhsergey <smovzhsergey@gmail.com>
2018-12-22 02:03:54 +0200

    add full answer

---
+++ build/precache-manifest.4f1baf92dd48b94d77681a1eb33916da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d87a6138a56850a086b",
+    "url": "/static/js/main.5d87a613.chunk.js"
+  },
+  {
+    "revision": "c4b81723801bca4663cb",
+    "url": "/static/js/1.c4b81723.chunk.js"
+  },
+  {
+    "revision": "5d87a6138a56850a086b",
+    "url": "/static/css/main.e6368280.chunk.css"
+  },
+  {
+    "revision": "5d6a33b1259e898eef13f74ea0c1c141",
+    "url": "/index.html"
+  }
+];
